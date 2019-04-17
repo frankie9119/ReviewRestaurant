92b9ff6627a08206c7a7efeaadae3d1cb84b9c46
William Turner <williamturner@Williams-Mac-mini.local>
2019-04-17 10:22:41 -0700

    save

--- js/app.js
+++ js/app.js
@@ -3,100 +3,12 @@
 // <script src="https://maps.googleapis.com/maps/api/js?key=YOUR_API_KEY&libraries=places">
 
 
-
 var map;
-
-function initMap() {
-  // Create the map.
-  var pyrmont = {
-    lat:51.5089022,
-    lng: -0.0990328
-  }; // Get initial map location
-  map = new google.maps.Map(document.getElementById('map'), {
-    center: pyrmont, // Set location
-    zoom: 17
-  });
-
-
-//___________________________________ BEGUIN Loop through Restaurants array
-
-      // Loop through markers
-      for(var i = 0;i < restaurants.length;i++){
-        // Add marker
-        addMarker(restaurants[i]);
-      }
-
-      // Add Marker Function
-      function addMarker(props){
-        var marker = new google.maps.Marker({
-          position:props.coords,
-          map:map,
-          //icon:props.iconImage
-        });
-        // Check content
-        if(props.content){
-          var infoWindow = new google.maps.InfoWindow({
-            content:props.content
-          });
-
-          marker.addListener('click', function(){
-            infoWindow.open(map, marker);
-          });
-        }
-      }
-
-// __________________________________________-END
-
-
-
-
-  // Create the places service.
-  var service = new google.maps.places.PlacesService(map);
-  var getNextPage = null;
-  var moreButton = document.getElementById('more');
-  moreButton.onclick = function() {
-    moreButton.disabled = true;
-    if (getNextPage) getNextPage();
-  };
-
-  // Perform a nearby search.
-  service.nearbySearch({
-      location: pyrmont,
-      radius: 500,
-      type: ['restaurant']
-    },
-    function(results, status, pagination) {
-      if (status !== 'OK') return;
-
-      createMarkers(results);
-      moreButton.disabled = !pagination.hasNextPage;
-      getNextPage = pagination.hasNextPage && function() {
-        pagination.nextPage();
-      };
-    });
-}
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 //________________________________________BEGIN create markers for type on line 31 "restaurant"
 
 function createMarkers(places) {
   var bounds = new google.maps.LatLngBounds();
-  var placesList = document.getElementById('places');
-
+ 
   for (var i = 0, place; place = places[i]; i++) {
     var image = {
       url: place.icon,
@@ -113,10 +25,6 @@ function createMarkers(places) {
       position: place.geometry.location
     });
 
-    var li = document.createElement('li');
-    li.textContent = place.name;
-    placesList.appendChild(li);
-
     bounds.extend(place.geometry.location);
   }
   map.fitBounds(bounds);
@@ -124,3 +32,53 @@ function createMarkers(places) {
 
 
 //________________________________________END create markers for type on line 26 "restaurant"
+
+function initMap() {
+  // Create the map.
+  var pyrmont = {
+    lat:51.5089022,
+    lng: -0.0990328
+  }; // Get initial map location
+  map = new google.maps.Map(document.getElementById('map'), {
+    center: pyrmont, // Set location
+    zoom: 17
+  });
+
+
+//___________________________________ BEGUIN Loop through Restaurants array
+
+
+// __________________________________________-END
+
+
+
+
+  // Create the places service.
+  var service = new google.maps.places.PlacesService(map);
+  var getNextPage = null;
+
+  // Perform a nearby search.
+  service.nearbySearch({
+      location: pyrmont,
+      radius: 500,
+      type: ['restaurant']
+    },
+    function(results, status, pagination) {
+      if (status !== 'OK') return;
+
+      createMarkers(results);
+     
+    });
+}
+
+
+
+
+
+
+
+
+
+
+
+
